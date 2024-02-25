69d7046b63ebc6652a7d9006b5820d44b2a88a5e
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-02-26 08:36:18 +0900

    Feat: chapter 3.3 test

--- study/springbatch/src/main/kotlin/kr/co/yunhalee/study/springbatch/SpringbatchApplication.kt
+++ study/springbatch/src/main/kotlin/kr/co/yunhalee/study/springbatch/SpringbatchApplication.kt
@@ -7,6 +7,15 @@ import org.springframework.boot.runApplication
 
 // 5.X 때 부터 해당 어노테이션이 없어야 spring batch job 실행
 //@EnableBatchProcessing
+
+// Spring batch repository setting
+@EnableBatchProcessing(
+//	dataSourceRef = "batchDataSource",
+//	transactionManagerRef = "batchTransactionManager",
+	tablePrefix = "SYSTEM_",
+	maxVarCharLength = 1000,
+	isolationLevelForCreate = "ISOLATION_REPEATABLE_READ"
+)
 @SpringBootApplication(nameGenerator = FullBeanNameGenerator::class)
 class SpringbatchApplication
 
--- study/springbatch/src/main/kotlin/kr/co/yunhalee/study/springbatch/configuration/batchconfigurer/CustomBatchConfiguration.kt
+++ study/springbatch/src/main/kotlin/kr/co/yunhalee/study/springbatch/configuration/batchconfigurer/CustomBatchConfiguration.kt
@@ -8,33 +8,37 @@ import org.springframework.batch.core.repository.support.JobRepositoryFactoryBea
 import org.springframework.boot.autoconfigure.batch.BatchProperties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
 import javax.sql.DataSource
 
-
-@Configuration
-@ConditionalOnProperty(Constants.PROPERTY_JOB_NAME, havingValue = BatchConfigurationConfiguration.JOB_NAME)
-class CustomBatchConfiguration(
-    private val dataSource: DataSource
-) : DefaultBatchConfiguration() {
-
-
-    override fun jobRepository(): JobRepository {
-
-        val factory = JobRepositoryFactoryBean()
-        factory.setDataSource(dataSource)
-        factory.transactionManager = getTransactionManager()
-        factory.setIsolationLevelForCreate("ISOLATION_SERIALIZABLE") // isolation 수준, 기본값은 “ISOLATION_SERIALIZABLE”
-
-        factory.setTablePrefix("BATCH_") // 테이블 Prefix, 기본값은 “BATCH_”,
-
-        // BATCH_JOB_EXECUTION 가 SYSTEM_JOB_EXECUTION 으로 변경됨
-        // 실제 테이블명이 변경되는 것은 아니다
-        // BATCH_JOB_EXECUTION 가 SYSTEM_JOB_EXECUTION 으로 변경됨
-        // 실제 테이블명이 변경되는 것은 아니다
-        factory.setMaxVarCharLength(1000) // varchar 최대 길이(기본값 2500)
-
-
-        return factory.getObject(); // Proxy 객체가 생성됨 (트랜잭션 Advice 적용 등을 위해 AOP 기술 적용)
-    }
-}
+//
+//@Configuration
+//@ConditionalOnProperty(Constants.PROPERTY_JOB_NAME, havingValue = BatchConfigurationConfiguration.JOB_NAME)
+//class CustomBatchConfiguration(
+//    private val dataSource: DataSource,
+//    private val transactionManager: PlatformTransactionManager
+//)  {
+//
+//
+//    @Bean
+//     fun jobRepository(): JobRepository {
+//
+//        val factory = JobRepositoryFactoryBean()
+//        factory.setDataSource(dataSource)
+//        factory.transactionManager = transactionManager
+//        factory.setIsolationLevelForCreate("ISOLATION_SERIALIZABLE") // isolation 수준, 기본값은 “ISOLATION_SERIALIZABLE”
+//
+//        factory.setTablePrefix("BATCH_") // 테이블 Prefix, 기본값은 “BATCH_”,
+//
+//        // BATCH_JOB_EXECUTION 가 SYSTEM_JOB_EXECUTION 으로 변경됨
+//        // 실제 테이블명이 변경되는 것은 아니다
+//        // BATCH_JOB_EXECUTION 가 SYSTEM_JOB_EXECUTION 으로 변경됨
+//        // 실제 테이블명이 변경되는 것은 아니다
+//        factory.setMaxVarCharLength(1000) // varchar 최대 길이(기본값 2500)
+//
+//
+//        return factory.getObject(); // Proxy 객체가 생성됨 (트랜잭션 Advice 적용 등을 위해 AOP 기술 적용)
+//    }
+//}
