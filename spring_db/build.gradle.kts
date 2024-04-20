7c636248e76bc33dbef5384e052ca668237c491a
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-04-20 10:04:26 +0900

    Feat: dependency 설정

--- spring_db/build.gradle.kts
+++ spring_db/build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.5")
+    // 엔진을 다운 받아서 보려면 spring이 알아서 버전을 맞춰 다운 받아준 버전을 external libraries 에서 h2database:h2:{2.2.224} 버전을 확인 해서 받아야 한다.
+    runtimeOnly("com.h2database:h2")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.32")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 }
