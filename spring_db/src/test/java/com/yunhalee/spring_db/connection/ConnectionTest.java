2a400f4e8a90f19b014bf9e628d7cc9e84e16eb2
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-04-27 01:06:49 +0900

    feat: 커넥션 풀 적용

--- spring_db/src/test/java/com/yunhalee/spring_db/connection/ConnectionTest.java
+++ spring_db/src/test/java/com/yunhalee/spring_db/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.yunhalee.spring_db.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -32,6 +33,44 @@ class ConnectionTest {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         // 동일하게 매번 다른 커넥션을 생성해서 가져오는걸 알 수 있지만, 설정과 사용이 분리된다.
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("My Pool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    @Test
+    void dataSourceConnectionPoolTimeout() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("My Pool");
+
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        Connection con3 = dataSource.getConnection();
+        Connection con4 = dataSource.getConnection();
+        Connection con5 = dataSource.getConnection();
+        Connection con6 = dataSource.getConnection();
+        Connection con7 = dataSource.getConnection();
+        Connection con8 = dataSource.getConnection();
+        Connection con9 = dataSource.getConnection();
+        Connection con10 = dataSource.getConnection();
+        Connection con11 = dataSource.getConnection();
+
+        // 커넥션 풀보다 많은 연결을 요청하고 기존에 연결을 반환하지 않기 때문에 timeout 발생
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
