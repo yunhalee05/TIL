e9fc0a7277e882e5f0e98707fa66c47d64d09444
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-04-29 23:25:28 +0900

    Feat: autocommit으로 인한 원자성 오류 발생 예외 케이스 구성

---
+++ spring_db/src/main/java/com/yunhalee/spring_db/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package com.yunhalee.spring_db.service;
+
+import com.yunhalee.spring_db.domain.Member;
+import com.yunhalee.spring_db.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public MemberServiceV1(MemberRepositoryV1 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
---
+++ spring_db/src/test/java/com/yunhalee/spring_db/service/MemberServiceV1Test.java
@@ -0,0 +1,80 @@
+package com.yunhalee.spring_db.service;
+
+import com.yunhalee.spring_db.domain.Member;
+import com.yunhalee.spring_db.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static com.yunhalee.spring_db.connection.ConnectionConst.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceV1Test {
+
+    private static final String MEMBER_A = "memberA";
+    private static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberB.getMemberId());
+        assertEquals(findMemberA.getMoney(), 8000);
+        assertEquals(findMemberB.getMoney(), 12000);
+    }
+
+
+    @Test
+    @DisplayName("이체중 예외 발생 : 기본 autocommit으로 중간단계만 commit")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        // when
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberB = memberRepository.findById(memberEx.getMemberId());
+        assertEquals(findMemberA.getMoney(), 8000);
+        assertEquals(findMemberB.getMoney(), 10000);
+    }
+
+}
