ffacd4de5d92f74f29076029dee0d2dc9d31df85
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-05-09 08:24:33 +0900

    Feat: 임베디드 메모리 테스트 디비 추가

--- spring_db_practice/src/main/java/com/yunhalee/spring_db_practice/SpringDbPracticeApplication.java
+++ spring_db_practice/src/main/java/com/yunhalee/spring_db_practice/SpringDbPracticeApplication.java
@@ -5,15 +5,22 @@ import com.yunhalee.spring_db_practice.config.JdbcTemplateV2Config;
 import com.yunhalee.spring_db_practice.config.JdbcTemplateV3Config;
 import com.yunhalee.spring_db_practice.config.MemoryConfig;
 import com.yunhalee.spring_db_practice.repository.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "com.yunhalee.spring_db_practice.web")
 public class SpringDbPracticeApplication {
+
+    private final Logger log = LoggerFactory.getLogger(SpringDbPracticeApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDbPracticeApplication.class, args);
@@ -23,5 +30,17 @@ public class SpringDbPracticeApplication {
     @Profile("local")
     public TestDataInit testDataInit(ItemRepository itemRepository) {
         return new TestDataInit(itemRepository);
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("메모리 데이터베이스 추가");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 }
