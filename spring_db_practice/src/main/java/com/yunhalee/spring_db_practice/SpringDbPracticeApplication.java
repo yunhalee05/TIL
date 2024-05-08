5039d0bb783038f5322d544a20252e8e1bb57b1f
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-05-09 08:28:01 +0900

    feat: 스프링 부트 테스트 DB 사용하도록 주석 처리

--- spring_db_practice/src/main/java/com/yunhalee/spring_db_practice/SpringDbPracticeApplication.java
+++ spring_db_practice/src/main/java/com/yunhalee/spring_db_practice/SpringDbPracticeApplication.java
@@ -32,15 +32,15 @@ public class SpringDbPracticeApplication {
         return new TestDataInit(itemRepository);
     }
 
-    @Bean
-    @Profile("test")
-    public DataSource dataSource() {
-        log.info("메모리 데이터베이스 추가");
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("메모리 데이터베이스 추가");
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 }
