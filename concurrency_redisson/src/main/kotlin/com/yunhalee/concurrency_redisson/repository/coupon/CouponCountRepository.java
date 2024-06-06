d5f19866fd5a38494ed7caa962041944269366b5
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-06-07 08:27:29 +0900

    feat: 쿠폰 카운트 레포 생성

---
+++ concurrency_redisson/src/main/kotlin/com/yunhalee/concurrency_redisson/repository/coupon/CouponCountRepository.java
@@ -0,0 +1,25 @@
+package com.yunhalee.concurrency_redisson.repository.coupon;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CouponCountRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public CouponCountRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Long increment() {
+        return redisTemplate
+                .opsForValue()
+                .increment("coupon_count");
+    }
+
+    public void clear() {
+        redisTemplate.delete("coupon_count");
+        redisTemplate.opsForValue().set("coupon_count", "0");
+    }
+}
--- concurrency_redisson/src/main/kotlin/com/yunhalee/concurrency_redisson/service/coupon/redis/RedissonCouponService.kt
+++ concurrency_redisson/src/main/kotlin/com/yunhalee/concurrency_redisson/service/coupon/redis/RedissonCouponService.kt
@@ -1,5 +1,8 @@
 package com.yunhalee.concurrency_redisson.service.coupon.redis
 
+import com.yunhalee.concurrency_redisson.domain.coupon.Coupon
+import com.yunhalee.concurrency_redisson.repository.coupon.CouponCountRepository
+import com.yunhalee.concurrency_redisson.repository.coupon.CouponRepository
 import com.yunhalee.concurrency_redisson.service.coupon.CouponService
 import org.redisson.api.RedissonClient
 import org.springframework.stereotype.Component
@@ -9,7 +12,9 @@ import java.util.concurrent.TimeUnit
 @Component
 class RedissonCouponService(
     private val redissonClient: RedissonClient,
-    private val couponService: CouponService
+    private val couponService: CouponService,
+    private val couponRepository: CouponRepository,
+    private val couponCountRepository: CouponCountRepository
 ) {
     fun issueCoupon(promotionId: Long, userId: Long, count: Int) {
         val lock = redissonClient.getLock(promotionId.toString())
@@ -19,7 +24,10 @@ class RedissonCouponService(
                 println("lock 획득에 실패하였습니다.")
                 return
             }
-            couponService.issueCoupon(userId, promotionId, count)
+            if (couponCountRepository.increment() > count) {
+                throw RuntimeException("이미 발급 가능한 쿠폰 갯수가 소진되었습니다.")
+            }
+            couponRepository.save(Coupon(userId = userId, promotionId = promotionId))
         } catch (e: InterruptedException) {
             throw RuntimeException("interruptedException이 발생했습니다.", e)
         } finally {
