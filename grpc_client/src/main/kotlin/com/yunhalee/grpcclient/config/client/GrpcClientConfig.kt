75094d53805047fefc8d7234dbc8ab75a4eee8a6
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2024-09-21 14:28:06 +0900

    feat: managedChannel을 이용해서 grpc service 빈으로 등록하기

--- grpc_client/src/main/kotlin/com/yunhalee/grpcclient/config/client/GrpcClientConfig.kt
+++ grpc_client/src/main/kotlin/com/yunhalee/grpcclient/config/client/GrpcClientConfig.kt
@@ -1,12 +1,17 @@
 package com.yunhalee.grpcclient.config.client
 
+import com.yunhalee.BookAuthorServiceGrpc
 import com.yunhalee.grpcclient.config.GrpcClientHeaderInterceptor
 import io.grpc.Channel
 import io.grpc.ClientInterceptors
+import io.grpc.ManagedChannel
+import io.grpc.Metadata
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
+import io.grpc.stub.MetadataUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.net.URI
 
 
 @Configuration
@@ -31,4 +36,39 @@ class GrpcClientConfig {
             ),
         )
     }
+//
+//    @Bean
+//    fun managedChannel(
+//        @Value("\${grpc.client.grpc-service.address}")
+//        address: String,
+//    ): ManagedChannel {
+//        val uri = URI.create(address)
+//        val useTransportSecurity = uri.scheme.equals("grpc+https", true)
+//        val channelBuilder = NettyChannelBuilder.forAddress(uri.host, uri.port)
+//            .defaultLoadBalancingPolicy("round_robin")
+//        if (useTransportSecurity) {
+//            channelBuilder.useTransportSecurity()
+//        } else {
+//            channelBuilder.usePlaintext()
+//        }
+//        return channelBuilder.build()
+//    }
+//
+//    @Bean
+//    fun metadata(): io.grpc.Metadata {
+//        val metadata = Metadata()
+//        val key = io.grpc.Metadata.Key.of("Authorization", io.grpc.Metadata.ASCII_STRING_MARSHALLER)
+//        metadata.put(key, "Bearer token")
+//        return metadata
+//    }
+//
+//
+//    @Bean
+//    fun bookService(managedChannel: ManagedChannel, metadata: Metadata): BookAuthorServiceGrpc.BookAuthorServiceBlockingStub {
+//        val stub = BookAuthorServiceGrpc.newBlockingStub(managedChannel)
+//        stub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(metadata))
+//        return stub
+//    }
+
+
 }
