992602aede50cbe3abfbd603587306edacca492d
Yunha Lee <63947424+yunhalee05@users.noreply.github.com>
2025-01-30 18:48:07 +0900

    feat: 오타 수정

--- msa_architecture/msa_architecture_service/infrastructure/kafka/kafka-producer/src/main/kotlin/com/yunhalee/msa/architecture/infrastructure/kafka/kafkaproducer/KafkaMessageHelper.kt
+++ msa_architecture/msa_architecture_service/infrastructure/kafka/kafka-producer/src/main/kotlin/com/yunhalee/msa/architecture/infrastructure/kafka/kafkaproducer/KafkaMessageHelper.kt
@@ -2,7 +2,6 @@ package com.yunhalee.msa.architecture.infrastructure.kafka.kafkaproducer
 
 import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
-import org.springframework.util.concurrent.ListenableFutureCallback
 import java.util.concurrent.CompletableFuture
 import java.util.logging.Logger
 
@@ -13,7 +12,7 @@ class KafkaMessageHelper {
     fun <T> getKafkaCallback(responseTopicName: String, avroModel: T, orderId: String?, avroModelName: String): CompletableFuture<SendResult<String, T>> {
         return CompletableFuture<SendResult<String, T>>().whenComplete{ result, exception ->
             if (exception != null) {
-                logger.warning("Error on kafka producer with key: $orderId message: ${avroModel.toString()} and exception: ${exception.message}"
+                logger.warning("Error on kafka producer with key: $orderId message: ${avroModel.toString()} and exception: ${exception.message}")
             } else {
                 logger.info("Received successful response from Kafka for order id: $orderId" +
                     " Topic: ${result!!.recordMetadata.topic()} Partition: ${result.recordMetadata.partition()} Offset: ${result.recordMetadata.offset()} Timestamp: ${result.recordMetadata.timestamp()}")
